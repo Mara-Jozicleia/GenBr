13b21584cc226ce1b4894dcb08ad84d5f670ad4c
Mara Joziclea <marajoziclea@gmail.com>
2021-07-27 18:35:21 -0300

    Projeto com Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+const routes: Routes = [
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path:'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'rodape', component: RodapeComponent},
+  {path: 'menu', component: MenuComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
