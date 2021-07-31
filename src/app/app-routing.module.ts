3e926140c5ecf872f12ae42d3fec2b1bedbc7e8f
Mara Joziclea <marajoziclea@gmail.com>
2021-07-31 19:54:07 -0300

    Componente inicio sem consumo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
 
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
   {path:'entrar', component: EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent},
   {path: 'rodape', component: RodapeComponent},
